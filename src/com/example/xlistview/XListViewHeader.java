dc5dc4c0ee604824d4ca7ef5ba5493adbe7fd1d3
superxuzj <271652260@qq.com>
2016-04-15 09:59:10 +0800

    first commit.

---
+++ src/com/example/xlistview/XListViewHeader.java
@@ -0,0 +1,134 @@
+/**
+ * @file XListViewHeader.java
+ * @create Apr 18, 2012 5:22:27 PM
+ * @author Maxwin
+ * @description XListView's header
+ */
+package com.example.xlistview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+/*XListViewHeader继承自linearLayout，用来实现下拉刷新时的界面展示，可以分为三种状态：正常、准备刷新、正在加载。
+在Linearlayout布局里面，主要有指示箭头、说明文本、圆形加载条三个控件。在构造函数中，调用了initView()
+进行控件的初始化操作。在添加布局文件的时候，指定高度为0，这是为了隐藏header，然后初始化动画，是为了完成箭头的旋转动作。
+setState()是设置header的状态，因为header需要根据不同的状态，完成控件隐藏、显示、改变文字等操作，
+这个方法主要是在XListView里面调用。除此之外，还有setVisiableHeight()和getVisiableHeight()，
+这两个方法是为了设置和获取Header中根布局文件的高度属性，从而完成拉伸和收缩的效果，而show()和hide()
+则显然就是完成显示和隐藏的效果。*/
+public class XListViewHeader extends LinearLayout {
+	private LinearLayout mContainer;
+	private ImageView mArrowImageView;
+	private ProgressBar mProgressBar;
+	private TextView mHintTextView;
+	private int mState = STATE_NORMAL;
+
+	private Animation mRotateUpAnim;
+	private Animation mRotateDownAnim;
+	
+	private final int ROTATE_ANIM_DURATION = 180;
+	
+	public final static int STATE_NORMAL = 0;
+	public final static int STATE_READY = 1;
+	public final static int STATE_REFRESHING = 2;
+
+	public XListViewHeader(Context context) {
+		super(context);
+		initView(context);
+	}
+
+	/**
+	 * @param context
+	 * @param attrs
+	 */
+	public XListViewHeader(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		initView(context);
+	}
+
+	private void initView(Context context) {
+		// 初始情况，设置下拉刷新view高度为0
+		LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+				LayoutParams.FILL_PARENT, 0);
+		mContainer = (LinearLayout) LayoutInflater.from(context).inflate(
+				R.layout.xlistview_header, null);
+		addView(mContainer, lp);
+		setGravity(Gravity.BOTTOM);
+
+		mArrowImageView = (ImageView)findViewById(R.id.xlistview_header_arrow);
+		mHintTextView = (TextView)findViewById(R.id.xlistview_header_hint_textview);
+		mProgressBar = (ProgressBar)findViewById(R.id.xlistview_header_progressbar);
+		
+		mRotateUpAnim = new RotateAnimation(0.0f, -180.0f,
+				Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+				0.5f);
+		mRotateUpAnim.setDuration(ROTATE_ANIM_DURATION);
+		mRotateUpAnim.setFillAfter(true);
+		mRotateDownAnim = new RotateAnimation(-180.0f, 0.0f,
+				Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+				0.5f);
+		mRotateDownAnim.setDuration(ROTATE_ANIM_DURATION);
+		mRotateDownAnim.setFillAfter(true);
+	}
+
+	public void setState(int state) {
+		if (state == mState) return ;
+		
+		if (state == STATE_REFRESHING) {	// 显示进度
+			mArrowImageView.clearAnimation();
+			mArrowImageView.setVisibility(View.INVISIBLE);
+			mProgressBar.setVisibility(View.VISIBLE);
+		} else {	// 显示箭头图片
+			mArrowImageView.setVisibility(View.VISIBLE);
+			mProgressBar.setVisibility(View.INVISIBLE);
+		}
+		
+		switch(state){
+		case STATE_NORMAL:
+			if (mState == STATE_READY) {
+				mArrowImageView.startAnimation(mRotateDownAnim);
+			}
+			if (mState == STATE_REFRESHING) {
+				mArrowImageView.clearAnimation();
+			}
+			mHintTextView.setText(R.string.xlistview_header_hint_normal);
+			break;
+		case STATE_READY:
+			if (mState != STATE_READY) {
+				mArrowImageView.clearAnimation();
+				mArrowImageView.startAnimation(mRotateUpAnim);
+				mHintTextView.setText(R.string.xlistview_header_hint_ready);
+			}
+			break;
+		case STATE_REFRESHING:
+			mHintTextView.setText(R.string.xlistview_header_hint_loading);
+			break;
+			default:
+		}
+		
+		mState = state;
+	}
+	
+	public void setVisiableHeight(int height) {
+		if (height < 0)
+			height = 0;
+		LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) mContainer
+				.getLayoutParams();
+		lp.height = height;
+		mContainer.setLayoutParams(lp);
+	}
+
+	public int getVisiableHeight() {
+		return mContainer.getLayoutParams().height;
+	}
+
+}
